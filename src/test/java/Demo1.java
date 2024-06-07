25128acdeeb4b4c811a9d2afcc8b3f6e18ef9b25
safak <safaktpkaya@gmail.com>
2024-06-07 02:25:27 +0200

    playwright commit

---
+++ src/test/java/Demo1.java
@@ -0,0 +1,12 @@
+import com.microsoft.playwright.*;
+
+public class Demo1 {
+    public static void main(String[] args){
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));//TRUE DERSEK BROWSER ACMADAN TEST ,FALSE OLURSA BROWSER ACIKKEN TEST
+
+        Page page  = browser.newPage();
+        page.navigate("https://www.google.com/");
+        System.out.println("Hello playwright");
+    }
+}
